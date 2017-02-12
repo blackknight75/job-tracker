355e578cd90c421523b15e3721b00b9311a7a564
blackknight75 <devbox4714@gmail.com>
2017-02-12 11:44:45 -0700

    add edit functionality to caegories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,13 +13,26 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def index
+    @categories = Category.all
+  end
+
   def show
     @category = Category.find(params[:id])
     @jobs = (@category.jobs).sort_by { |job| job.title}
   end
 
-  def index
-    @categories = Category.all
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      redirect_to categories_path
+    else
+      render :edit
+    end
   end
 
   def destroy
